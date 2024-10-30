f8a3dc59d017d146e2c928c95f376e56277d7d62
Fabio <90531115+fanaia@users.noreply.github.com>
2024-10-30 12:07:41 -0300

    Refactor apiRetaguarda.js to update base URL

--- ui-retaguarda/src/config/apiRetaguarda.js
+++ ui-retaguarda/src/config/apiRetaguarda.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRetaguarda = axios.create({
-  baseURL: "http://127.0.0.1:3010/api",
+  baseURL: "http://209.38.6.161:3010/api",
+  // baseURL: "http://127.0.0.1:3010/api",
 });
 
 apiRetaguarda.interceptors.request.use((config) => {
